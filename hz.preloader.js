36d95604c736902cc4d156ef4bdc64798c585492
LionzHunter <hshowmail@gmail.com>
2017-03-04 22:14:44 +0000

    rand ids and more twiks

--- hz.preloader.js
+++ hz.preloader.js
@@ -2,27 +2,38 @@
  * Created by HAMZA on 28/02/2017.
  */
 (function ( $ ) {
-    var CurrentColor=0;
-    var Colors=['#51BBA7','#F44336', '#9C27B0', '#E91E63'];
-    var trigger=false;
+    var CurrentColor=0,
+    Colorz=[],
+    trigger=false,
+    loadID=randID(),
+    innerID=randID(),
+    containerID=randID();
+
+    var exStyle='@keyframes loading {0% {transform: rotate(0);}100% {transform: rotate(360deg);}}' +
+                '@keyframes loading-circle {0% {stroke-dashoffset: 10;}100% {stroke-dashoffset: -800;}}';
+    var exHtml='<div id="'+containerID+'">' +
+                '<svg id="'+loadID+'" x="0px" y="0px" viewBox="0 0 150 150">' +
+                '<circle id="'+innerID+'" cx="75" cy="75" r="60"/>' +
+                '</svg>'+
+                '</div>'
+
+
+
+
 
     function initializ(container){
+        
         $("head").append('<style type="text/css"></style>');
         var newStyleElement = $("head").children(':last');
-        newStyleElement.html('@keyframes loading {0% {transform: rotate(0);}100% {transform: rotate(360deg);}}' +
-            '@keyframes loading-circle {0% {stroke-dashoffset: 10;}100% {stroke-dashoffset: -800;}}');
-        container.append('<div id="loading-container">' +
-            '<svg id="load" x="0px" y="0px" viewBox="0 0 150 150">' +
-            '<circle id="loading-inner" cx="75" cy="75" r="60"/>' +
-            '</svg>'+
-            '</div>');
+        newStyleElement.html(exStyle);
+        container.append(exHtml);
 
-        $('#load').css({
+        $('#'+loadID).css({
             width: '50px',
             animation: 'loading 3s linear infinite'
         });
 
-        $('#loading-inner').css({
+        $('#'+innerID).css({
             'stroke-dashoffset': '0',
             'stroke-dasharray': '450',
             'stroke-width': '10',
@@ -32,24 +43,20 @@
             'stroke': '#E91E63',
             'fill': 'transparent'
         });
-        $('#loading-container').css({
+        $('#'+containerID).css({
             'display': 'none',
             'position': 'fixed',
             'height': '50px',
             'width': '50px',
             'margin': '0 auto',
             'top': '5%',
-            'left': '48%'
+            'left': ($(document).width/2)
         });
     }
     function Coloriz(){
-
-
-
-        $('#loading-inner').css({
-            'stroke': Colors[CurrentColor]
-
-        });
+        $('#'+innerID).css({
+            'stroke': Colorz[CurrentColor]
+            });
         setTimeout(Coloriz, 2000);
         if(CurrentColor<3){CurrentColor++;}else{CurrentColor=0;}
     }
@@ -59,27 +66,29 @@
 
 
     $.fn.hzPreloader = function( options ) {
-
         // default options fi7alat ma.
         var settings = $.extend({
             container: "body",
-            auto: true
+            auto: true,
+            colors: ['#51BBA7','#F44336', '#9C27B0', '#E91E63']
         }, options );
+
+        Colorz=settings.colors;
+
 
 
         if(!exists()){
             initializ(this);
             trigger=settings.auto;
-
-        }
+            }
 
 
 
         if(options=='show'){
-            $('#loading-container').show();
+            $('#'+containerID).show();
             return this;
         }else if(options=='hide'){
-            $('#loading-container').fadeOut('slow').remove();
+            $('#'+containerID).fadeOut('slow').remove();
             return this;
         }
 
@@ -91,13 +100,22 @@
 
 
         setTimeout(Coloriz, 1000);
-        if (trigger){ $('#loading-container').show();}
+        if (trigger){ $('#'+containerID).show();}
         return this;
     };
 
 
     function exists(){
-        return (this.find('#loading-container').length>0)
+        return (this.find('#'+containerID).length>0)
+    }
+    function randID(){
+    var text = "hzpl_";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
     }
 
 }( jQuery ));
